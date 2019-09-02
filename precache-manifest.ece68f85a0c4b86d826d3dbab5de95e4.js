0a1867c6c2f137fcace95352d049d7db3e1d58ef
jonsigurd <jonsigurdvp@gmail.com>
2019-09-02 05:40:42 +0000

    Updates

---
+++ precache-manifest.ece68f85a0c4b86d826d3dbab5de95e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e15e7cfb1648cc1d7231052066ea553c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8790559f4d126dc89ce4",
+    "url": "/monsters-rolodex/static/css/main.9ea78a26.chunk.css"
+  },
+  {
+    "revision": "f478c3e6a5a59dee73f6",
+    "url": "/monsters-rolodex/static/js/2.81f98875.chunk.js"
+  },
+  {
+    "revision": "8790559f4d126dc89ce4",
+    "url": "/monsters-rolodex/static/js/main.50325e84.chunk.js"
+  },
+  {
+    "revision": "9ab012cb21ad84bf1419",
+    "url": "/monsters-rolodex/static/js/runtime~main.d10d2276.js"
+  }
+]);
